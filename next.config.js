1deee1882139ee5397b455546248422f713c014b
Kitsana <folklove45@gmail.com>
2022-02-28 04:25:57 +0700

    - fix config next

--- next.config.js
+++ next.config.js
@@ -6,8 +6,10 @@ const nextConfig = {
   assetPrefix: isProd ? '/kaidee-pre-test/' : '',
   reactStrictMode: true,
   images: {
-    loader: "imgix",
-    path: ""
+    loader: 'imgix',
+    path: isProd
+    ? 'https://folksongdevelop.github.io/kaidee-pre-test/'
+    : 'http://localhost:3000/'
   }
 }
 
